005afa095cd3e692d71cb386fe025f1c49c5ed39
Giorubio27 <Giorubio27@gmail.com>
2026-04-09 11:18:23 -0600

    I imported my getUfcRankings

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,11 @@
 import { loadHeaderFooter } from "./utils/utils.mjs";
+import { getUfcRankings } from "./services/fighterData";
 
 
 loadHeaderFooter();
+
+async function initRankings() {
+    const data = await getUfcRankings();
+
+};
+initRankings();
